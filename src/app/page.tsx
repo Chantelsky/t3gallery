57ebecb30d9b1df7f5205b1677da912548b9f31b
Chantel Punt <csky.punt@gmail.com>
2024-11-16 10:26:30 +1100

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/X5UnS7lIqS2xBsZGubF6EbO8jroHwTlu1GM4hgFRIi3X9AsL",
+  "https://utfs.io/f/X5UnS7lIqS2xgZdx1yrzdLIG1KPqpfy3DBwvXjtsSU8AbkFY",
+  "https://utfs.io/f/X5UnS7lIqS2xU6q5aIiFrdpDzOSfRBi9kZxAm5cXsjUbTqva",
+  "https://utfs.io/f/X5UnS7lIqS2xj3eOYv14AlPxSYT6iHVOcu5q2QsmvL9Gw0Xk",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       Hello (gallery in progress)
     </main>
   );
